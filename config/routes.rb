b448eb79b6443691bd3afca0e802f573b4743497
romigos <bolistovskiyroman@gmail.com>
2020-09-23 17:16:16 +0300

    create posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :authors
+  get 'home/index'
+  root to: "home#index"
+  scope module: 'authors' do
+    resources :posts
+  end
 end
